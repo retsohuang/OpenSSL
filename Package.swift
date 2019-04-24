1109e6be2f8e37643a22148f2fbd668be242c1c7
Retso Huang <retsohuang@gmail.com>
2019-04-24 17:50:28 +0800

    chore: Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "OpenSSL",
+    // platforms: [.iOS("8.0")],
+    products: [
+      .library(
+        name: "OpenSSL",
+        targets: ["OpenSSL (iOS)"]
+      )
+    ],
+    dependencies: [
+    ],
+    targets: [
+      .target(
+        name: "OpenSSL (iOS)"
+      )
+    ]
+)
